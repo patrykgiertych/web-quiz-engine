e9218cd6376a45e136d55aad4c18e68f4c246ef1
patrykgiertych <patrykgiertych1@gmail.com>
2021-10-23 22:38:21 +0200

    user can create his own quiz questions, view all quizzes, solve a quiz

--- src/main/java/engine/quiz/Quiz.java
+++ src/main/java/engine/quiz/Quiz.java
@@ -1,5 +1,6 @@
 package engine.quiz;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,13 +10,21 @@ import lombok.Setter;
 @Setter
 public class Quiz {
 
+    private int id;
     private String title;
     private String text;
     private String[] options;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
 
-    public Quiz(String title, String text, String[] options) {
+    public Quiz(String title, String text, String[] options, int answer) {
         this.title = title;
         this.text = text;
         this.options = options;
+        this.answer = answer;
+    }
+
+    public boolean isCorrect(int userAnswer) {
+        return this.answer == userAnswer;
     }
 }
--- src/main/java/engine/quiz/QuizController.java
+++ src/main/java/engine/quiz/QuizController.java
@@ -1,26 +1,59 @@
 package engine.quiz;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/quiz")
+@RequestMapping("/api/")
 public class QuizController {
 
-    private final Quiz quiz = new Quiz(
-            "The Java Logo",
-            "What is depicted on the Java logo?",
-            new String[]{"Robot", "Tea leaf", "Cup of coffee", "Bug"}
-    );
+    private List<Quiz> quizzes = new ArrayList<>();
+    private int counter = 1;
 
-
-    @GetMapping
-    public Quiz getQuiz() {
+    @PostMapping("quizzes")
+    public Quiz addQuiz(@RequestBody Quiz quiz) {
+        quiz.setId(counter);
+        counter++;
+        quizzes.add(quiz);
         return quiz;
     }
 
-    @PostMapping
-    public Answer getAnswer(@RequestParam("answer") int answer) {
-        if (answer == 2) {
+    @GetMapping("quizzes/{id}")
+    public Quiz getQuiz(@PathVariable int id) {
+        Quiz quizById = null;
+        for (Quiz q : quizzes) {
+            if (q.getId() == id) {
+                quizById = q;
+            }
+        }
+        if (quizById == null)
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        return quizById;}
+
+    @GetMapping("quizzes")
+    public List<Quiz> getAllQuizzes() {
+        return quizzes;
+    }
+
+    @PostMapping("quizzes/{id}/solve")
+    public Answer solveQuiz(@RequestParam("answer") int answer, @PathVariable int id) {
+        Quiz quiz = null;
+        for (Quiz q : quizzes) {
+            if (q.getId() == id) {
+                quiz = q;
+            }
+        }
+        if (quiz == null)
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        if (quiz.isCorrect(answer)) {
             return new Answer(true);
         }
         return new Answer(false);
