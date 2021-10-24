a0537a27f808a3cda0ad990b4f9eb808f878c683
patrykgiertych <patrykgiertych1@gmail.com>
2021-10-24 13:16:10 +0200

    implemented h2 database, created QuizRepository

--- src/main/java/engine/quiz/Quiz.java
+++ src/main/java/engine/quiz/Quiz.java
@@ -5,6 +5,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -13,9 +17,12 @@ import java.util.Arrays;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class Quiz {
 
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     @NotEmpty
     private String title;
     @NotEmpty
--- src/main/java/engine/quiz/QuizController.java
+++ src/main/java/engine/quiz/QuizController.java
@@ -1,5 +1,6 @@
 package engine.quiz;
 
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,53 +11,38 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/")
+@AllArgsConstructor
 public class QuizController {
 
-    private List<Quiz> quizzes = new ArrayList<>();
-    private int counter = 1;
+    private final QuizRepository quizRepository;
 
     @PostMapping("quizzes")
     public Quiz addQuiz(@Valid @RequestBody Quiz quiz) {
-        quiz.setId(counter);
-        counter++;
         if (quiz.getAnswer() == null) {
             quiz.setAnswer(new int[]{});
         }
-        quizzes.add(quiz);
+        quizRepository.save(quiz);
         return quiz;
     }
 
     @GetMapping("quizzes/{id}")
-    public Quiz getQuiz(@PathVariable int id) {
-        Quiz quizById = null;
-        for (Quiz q : quizzes) {
-            if (q.getId() == id) {
-                quizById = q;
-            }
-        }
-        if (quizById == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "entity not found"
-            );
-        return quizById;}
+    public Quiz getQuiz(@PathVariable Long id) {
+        Quiz quiz = quizRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found")
+        );
+        return quiz;
+    }
 
     @GetMapping("quizzes")
     public List<Quiz> getAllQuizzes() {
-        return quizzes;
+        return quizRepository.findAll();
     }
 
     @PostMapping("quizzes/{id}/solve")
-    public Result solveQuiz(@PathVariable int id, @RequestBody Answer answer) {
-        Quiz quiz = null;
-        for (Quiz q : quizzes) {
-            if (q.getId() == id) {
-                quiz = q;
-            }
-        }
-        if (quiz == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND, "entity not found"
-            );
+    public Result solveQuiz(@PathVariable Long id, @RequestBody Answer answer) {
+        Quiz quiz = quizRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found")
+        );
         if (quiz.isCorrect(answer)) {
             return new Result(true);
         }
